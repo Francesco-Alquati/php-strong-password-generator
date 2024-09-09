af5d3be013a0a7681cb5c7f5143271d3a88bf933
Francesco-Alquati <francesco.alquati@tiscali.it>
2024-09-09 13:04:25 +0200

    [UPD] Added some conditions and styles

--- function.php
+++ function.php
@@ -10,8 +10,17 @@
         return $password;
     }
 
-    if (isset($_GET['passwordLength'])) {
+    if (isset($_GET['passwordLength']) && $_GET['passwordLength'] != '') {
         $lunghezza = intval($_GET['passwordLength']);
         $password = generaPassword($lunghezza);
+
+        // Verifica del numero minimo di caratteri
+        if(intval($_GET['passwordLength']) < 8){
+            $password = '<span class="bg-icons"><i class="fa-solid text-warning fa-lg fa-triangle-exclamation"></i></span><span class="text-danger ms-3 me-3 text-uppercase">Seleziona il numero minimo di caratteri (8)</span><span class="bg-icons"><i class="fa-solid text-warning fa-lg fa-triangle-exclamation"></i></span>';
+        }
+    }
+    // Controllo se l'utente ha selezionato un numero di caratteri 
+    else {
+        $password = '<span class="bg-icons"><i class="fa-solid text-warning fa-lg fa-triangle-exclamation"></i></span><span class="text-danger ms-3 me-3 text-uppercase">Seleziona un numero di caratteri</span><span class="bg-icons"><i class="fa-solid text-warning fa-lg fa-triangle-exclamation"></i></span>';
     }
 ?>
--- index.php
+++ index.php
@@ -9,6 +9,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./css/style.css">
     <title>Password Generator</title>
 </head>
@@ -19,17 +20,17 @@
             <div class="col-12 text-center">
                 <div class="card-header mt-5 rounded">
                     <h1 class="text-white">Strong Password Generator</h1>
-                    <h3 class="text-white mt-4">Genera una password <span class="text-green text-uppercase ms-2">Sicura</span></h3>
+                    <h3 class="text-white mt-4">Genera una password <span class="text-green text-uppercase ms-2">Sicura</span> <i class="fa-solid fa-lock text-black ms-2"></i></h3>
                 </div>
             </div>
         </div>
         <div class="row mt-5">
             <div class="col-12">
                 <div class="form-container">
-                    <form action="./index.php" method="GET">
+                    <form action="./index.php" method="get">
                         <div class=" mt-3 d-flex justify-content-around align-items-center">
                             <label class="form-label fs-4 text-white text-uppercase mt-4">Lunghezza Password :</label>
-                            <input type="number" class="label-size password " id="passwordLength" name="passwordLength" min="8" max="80">
+                            <input type="number" class="label-size password " id="passwordLength" placeholder="lunghezza password" name="passwordLength" min="8" max="80">
                         </div>
                         <div class="text-center mt-5">
                             <input type="submit" class="btn btn-warning" value="Genera">
